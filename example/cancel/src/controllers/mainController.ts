bb76fcfbd6fdb4ed84a0ffd3ef39a132506fde0c
xernois <antoine2496@gmail.com>
2023-05-26 16:59:11 +0200

    [📝] Added cancel example

--- example/cancel/src/controllers/mainController.ts
+++ example/cancel/src/controllers/mainController.ts
@@ -6,11 +6,14 @@ export default class MainController {
     @Route({ path: '/api', method: [Method.GET], name: 'index' })
     public async index(req: Request, res: Response) {
 
+        let timeout: NodeJS.Timeout;
+
         req.on('close', () => {
             console.log('request was cancelled');
+            clearTimeout(timeout);
         });
 
-        setTimeout(() => {
+        timeout = setTimeout(() => {
             res.end(JSON.stringify({ message: 'Hello world' }));
             console.log('this shouldn\'t log when the request is cancelled')
         }, 2000);
